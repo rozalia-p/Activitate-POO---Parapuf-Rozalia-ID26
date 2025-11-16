e79b636121cccfca9d77232e2f12d93634f850ef
Rozalia <rozalia1337@gmail.com>
2025-11-16 18:15:45 +0200

    update procesor

--- main.cpp
+++ main.cpp
@@ -5,61 +5,107 @@
 using namespace std;
 
 
+class Procesor;
+class RAM;
+class Interfata;
+
+float calculeazaIndicePerformanta(Procesor& p, RAM& r);
+bool esteSistemDeGaming(Procesor& p, RAM& r, Interfata& i);
+
+
 class Procesor {
+private:
+	string model;
+	int nrNuclee;
+	float frecventaGHz;
+	const int anLansare;
 	static int totalProcesoare;
-	const string arhitectura;
-	int nuclee;
-	string producator;
-	double frecventaGHz;
-	double* temperatura;
+	float* temperaturi;
+	int nrTemperaturi;
+
 public:
-
-	//constructor implicit
-	Procesor() : arhitectura(""){
-		this->nuclee = 0;
+	Procesor() :anLansare(0) {
+		this->model = "Necunoscut";
+		this->nrNuclee = 0;
 		this->frecventaGHz = 0;
-		this->producator = "Necunoscut";
-		this->temperatura = NULL;
+		this->nrTemperaturi = 0;
+		this->temperaturi = NULL;
+		totalProcesoare++;
 	}
 
-	//constructor cu un parametru
-	Procesor() :arhitectura(arhitectura) {
-		this->nuclee = 0;
-		this->frecventaGHz = 0;
-		this->temperatura = NULL;
-		this->producator = "Necunoscut";
+	Procesor(string model) :anLansare(0) {
+		this->model = model;
+		this->nrNuclee = 4;
+		this->frecventaGHz = 3.0f;
+		this->temperaturi = NULL;
+		totalProcesoare++;
 	}
-
-
-	//constructor cu toti parametrii
-	Procesor(int nuclee, string producator, double frecventaGHz, double* temperatura):arhitectura(arhitectura) {
-		this->nuclee = nuclee;
+	Procesor(string model, int nrNuclee, float frecventaGHz, int anLansare, int nrTemperaturi, float* temperaturi) : anLansare(anLansare) {
+		this->model = model;
+		this->nrNuclee = nrNuclee;
 		this->frecventaGHz = frecventaGHz;
-		this->temperatura = new double[this->totalProcesoare];
-		for (int i = 0; i < this->totalProcesoare; i++) {
-			this->temperatura[i] = temperatura[i];
+		this->nrTemperaturi = nrTemperaturi;
+		if (this->nrTemperaturi > 0 && temperaturi != NULL) {
+			this->temperaturi = new float[this->nrTemperaturi];
+			for (int i = 0; i < this->nrTemperaturi; i++) {
+				this->temperaturi[i] = temperaturi[i];
+			}
 		}
-		this->producator = producator;
+		else {
+			this->temperaturi = NULL;
+		}
+		totalProcesoare++;
+	}
+
+	Procesor(Procesor& p) : anLansare(p.anLansare) {
+		this->model = p.model;
+		this->nrNuclee = p.nrNuclee;
+		this->frecventaGHz = p.frecventaGHz;
+		this->nrTemperaturi = p.nrTemperaturi;
+		if (this->nrTemperaturi > 0 && p.temperaturi != NULL) {
+			this->temperaturi = new float[this->nrTemperaturi];
+			for (int i = 0; i < this->nrTemperaturi; i++) {
+				this->temperaturi[i] = p.temperaturi[i];
+			}
+		}
+		else {
+			this->temperaturi = NULL;
+		}totalProcesoare++;
 	}
 
 	~Procesor() {
-		if (this->temperatura != NULL) {
-			delete[] this->temperatura;
+		if (this->temperaturi != NULL) {
+			delete[] this->temperaturi;
 		}
 	}
-	static double scorPerformanta(double frecventaGHz, int nuclee) {
-		return frecventaGHz * nuclee * 1000.0;
+	//getteri
+	string getModel() {
+		return this->model;
+	}
+	int getNrNuclee() {
+		return this->nrNuclee;
+	}
+	float getFrecventaGHz() {
+		return this->frecventaGHz;
+	}
+	int getAnLansare() {
+		return this->anLansare;
+	}
+	int getNrTemperaturi() {
+		return this->nrTemperaturi;
+	}
+	float* getTemperaturi() {
+		return this->temperaturi;
+	}
+	//setteri
+	void setModel(string modelNou) {
+		this->model = modelNou;
+	}
+	void setNrNuclee(int nrNucleeNou) {
+		this->nrNuclee = nrNucleeNou;
+	}
+	void setFrecventaGHz(float frecventaGHzNou) {
+		this->frecventaGHz = frecventaGHzNou;
 	}
 
-	friend ostream& operator<<(ostream& out, const Procesor& obj) {
-		out << "Arhitectura procesor: " << obj.arhitectura << endl;
-		out << "Productor: " << obj.producator << endl;
-		out << "Nuclee: " << obj.nuclee << endl;
-		out << "Frecventa: " << obj.frecventaGHz << endl;
-
-
-	}
-	
 };
-
-int Procesor::totalProcesoare = 0;
